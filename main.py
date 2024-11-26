68a24c9a88fc732c4915cac8272e5ad28e289491
AlexGromov63 <alexgromov6363@gmail.com>
2024-11-26 19:21:34 +0300

    clear ui file and random color

--- main.py
+++ main.py
@@ -1,21 +1,40 @@
 import sys
 from random import randint
-from PyQt6.uic import loadUi
 from PyQt6.QtWidgets import QApplication, QWidget
 from PyQt6.QtGui import QColor, QPen, QPainter
 from PyQt6.QtCore import QPointF
+from PyQt6 import QtCore, QtWidgets
 
 
-class MyWidget(QWidget):
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(571, 445)
+        self.pushButton = QtWidgets.QPushButton(parent=Form)
+        self.pushButton.setGeometry(QtCore.QRect(180, 180, 201, 41))
+        self.pushButton.setStyleSheet("background-color: #ffffff")
+        self.pushButton.setObjectName("pushButton")
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.pushButton.setText(_translate("Form", "Create yellow circle"))
+
+
+class MyWidget(QWidget, Ui_Form):
     def __init__(self):
         super().__init__()
-        loadUi('UI.ui', self)
+        self.setupUi(self)
         self.flag = False
         self.show()
         self.pushButton.clicked.connect(self.run)
 
     def run(self):
         self.radius = randint(10, int(min([self.size().width(), self.size().height()]) / 2))
+        self.color = QColor(randint(0, 255), randint(0, 255), randint(0, 255))
         self.flag = True
         self.update()
 
@@ -23,7 +42,7 @@ class MyWidget(QWidget):
         if self.flag:
             cordsX = sorted([self.radius, self.size().width() - self.radius])
             cordsY = sorted([self.radius, self.size().height() - self.radius])
-            pen = QPen(QColor(255, 255, 0))
+            pen = QPen(self.color)
             pain = QPainter(self)
             pain.setPen(pen)
             pain.drawEllipse(QPointF(randint(cordsX[0], cordsX[1]), 
